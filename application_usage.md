a4bb4834aa8ed72d18a84a9142cfc51d7d346baa
archu2000 <achukala2000@gmail.com>
2022-10-28 18:28:08 +0530

    using bootstrap to load css

---
+++ application_usage.md
@@ -0,0 +1,9 @@
+After cloning the git repo:
+run ```npm i node-modules```
+-- that installs the packages required to run the application
+
+To use bootstrap:
+run ```npm install jquery --save```
+run ```npm install popper.js --save```
+run ```npm install bootstrap --save```
+-- install of these packages help react use it by importing
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 //inorder to render react element into the reactDOM
 import ReactDOM from 'react-dom/client';
+//to import bootstrap and make it fully work
+import "jquery";
+import "popper.js/dist/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-var element = <h1>Hello People!</h1>
+//btn btn-danger is a predefined css class comes from bootstrap.css
+var element = <button class="btn btn-info">Hello People!</button>
 //element = Hello World!
 // document.getElementByID("root") will select element that renders
 //inoder to select the element by ID, use root since already predefined in index.html
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
 // root.render(<h1>Welcome to my React App!</h1>)  //either hard-code or call the value
+
+console.log(element)
